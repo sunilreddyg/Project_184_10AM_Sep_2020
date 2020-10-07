31f1036314cbc301966a138c9cfc45f7ebd4c515
SUNIL <SUNIL@192.168.1.3>
2020-10-07 11:52:07 +0530

    LinkText

---
+++ Project_184/src/selenium_webdriver/Link/Example_linkText.java
@@ -0,0 +1,37 @@
+package selenium_webdriver.Link;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Example_linkText {
+
+	public static void main(String[] args) throws Exception {
+		
+		System.setProperty("webdriver.chrome.driver", "Drivers\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.icicidirect.com/");
+		driver.manage().window().maximize();
+		
+		
+		driver.findElement(By.linkText("View Webinars")).click();
+		
+		/*
+		 * <a class="tab-active" > ICICIdirect     Money Manager</a>
+         *    => Use partial linktext when linkname have space           
+		 */
+		driver.findElement(By.partialLinkText("Money Manager")).click();
+		
+		
+		driver.findElement(By.linkText("Study Table")).click();
+		Thread.sleep(2000);
+		
+		
+		driver.findElement(By.linkText("Insurance")).click();
+		
+		
+		driver.findElement(By.linkText("Learn About The Importance Of Health Insurance")).click();
+		
+	}
+
+}
